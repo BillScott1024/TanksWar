adf6fd2b618b46b8808a25773223e9a7874aea95
BillScott1024 <1355542655@qq.com>
2018-01-20 14:08:27 +0800

    Init Project Files.

---
+++ Assets/Scripts/TankHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TankHealth : MonoBehaviour {
+
+    public int hp = 100;
+    public GameObject tankExplosition;
+
+    public AudioClip tankExplosionAudio;
+
+    public Slider hpSlider;
+
+    private int hpTotal;
+	// Use this for initialization
+	void Start ()
+	{
+	    hpTotal = hp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TakeDamage()
+    {
+        if (hp <= 0) return;
+        hp -= Random.Range(10, 20);
+       hpSlider.value = (float)hp / hpTotal;
+        if (hp <= 0)
+        {
+            //死亡
+            AudioSource.PlayClipAtPoint(tankExplosionAudio,transform.position);
+            GameObject.Instantiate(tankExplosition, transform.position, transform.rotation);
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/shell.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shell : MonoBehaviour {
+
+    public GameObject shellExplosionPrefab;
+
+    public AudioClip shellExplosionAudio;
+	// Use this for initialization
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        AudioSource.PlayClipAtPoint(shellExplosionAudio,transform.position);
+        GameObject.Instantiate(shellExplosionPrefab, transform.position, transform.rotation);
+        GameObject.Destroy(this.gameObject);
+
+
+        if (collider.tag == "Tank")
+        {
+            collider.SendMessage("TakeDamage");
+        }
+    }
+}
